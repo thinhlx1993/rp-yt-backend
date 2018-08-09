5459c0d8100cdc0ab667944e60dc333ec9c880e2
thinh <thinh@boot.ai>
2018-08-09 18:56:08 +0700

    add file

---
+++ untils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+def find_report_link(s):
+    first = 'https://www.youtube.com/channel/'
+    try:
+        start = s.index(first) + len(first) + 2
+        report_link = 'https://www.youtube.com/reportabuse?u={}'.format(s[start:])
+        return report_link
+    except ValueError:
+        return None
+    
+def watch_videos(browser, href):
+    ActionChains(browser) \
+    .key_down(Keys.CONTROL) \
+    .send_keys('t') \
+    .key_up(Keys.CONTROL) \
+    .perform()
+    browser.get(href)
+        
+    
+            
