d2dc82986217f66e956689c19d367d30e855eccc
thinh <thinh@boot.ai>
2018-08-13 17:29:53 +0700

    update models

---
+++ models/multi_classification.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+tf.__version__
+import sys
+import os
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense, Activation
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras import callbacks
+from livelossplot import PlotLossesKeras
+
+train_data_path = './dataset/training_set'
+validation_data_path = './dataset/test_set'
+
+"""
+Parameters
+"""
+img_width, img_height = 150, 150
+batch_size = 32
+nb_train_samples = 3946
+nb_validation_samples = 642
+samples_per_epoch = 8000
+validation_steps = 300
+nb_filters1 = 32
+nb_filters2 = 64
+conv1_size = 3
+conv2_size = 2
+pool_size = 2
+classes_num = 2
+lr = 1e-4
+momentum = 0.9
+epochs = 25
+
+model = Sequential()
+model.add(Convolution2D(nb_filters1, conv1_size, conv1_size, border_mode ="same", input_shape=(img_width, img_height, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size)))
+
+model.add(Convolution2D(nb_filters2, conv2_size, conv2_size, border_mode ="same"))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(pool_size, pool_size), dim_ordering='th'))
+
+model.add(Flatten())
+model.add(Dense(256))
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
+model.add(Dense(classes_num, activation='softmax'))
+
+#model.compile(loss='categorical_crossentropy',
+#              optimizer=optimizers.SGD(lr=lr, momentum=momentum),
+#              metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=lr),
+              metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_path,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_path,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='categorical')
+
+"""
+Tensorboard log
+"""
+#log_dir = './tf-log/'
+#tb_cb = callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+#cbks = [tb_cb]
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    callbacks=[PlotLossesKeras()],
+    validation_steps=nb_validation_samples // batch_size,)
+
+target_dir = './models/'
+if not os.path.exists(target_dir):
+  os.mkdir(target_dir)
+model.save('./models/model.h5')
+model.save_weights('./models/weights.h5')
+
+train_generator.class_indices
---
+++ models/test.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+
+img_width, img_height = 150, 150
+model_path = './models/model.h5'
+model_weights_path = './models/weights.h5'
+model = load_model(model_path)
+model.load_weights(model_weights_path)
+
+def predict(file):
+    x = load_img(file, target_size=(img_width,img_height))
+    x = img_to_array(x)
+    x = np.expand_dims(x, axis=0)
+    array = model.predict(x)
+    result = array[0]
+    answer = np.argmax(result)
+#    print(answer)
+#    if answer == 0:
+#        print("Label: bicycles")
+#    elif answer == 1:
+#        print("Labels: bridges")
+#    elif answer == 2:
+#        print("Label: bus")
+#    elif answer == 3:
+#        print("Label: motorcycles")
+#    elif answer == 4:
+#        print("Label: mountains_or_hills")
+#    elif answer == 5:
+#        print("Label: statues")
+#    elif answer == 6:
+#        print("Label: storefront")
+
+    return answer
+
+bicycles_t = 0
+bicycles_f = 0
+bridges_t = 0
+bridges_f = 0
+bus_t = 0
+bus_f = 0
+motorcycles_t = 0
+motorcycles_f = 0
+mountains_or_hills_t = 0
+mountains_or_hills_f = 0
+statues_t = 0
+statues_f = 0
+storefront_t = 0
+storefront_f = 0
+
+for i, ret in enumerate(os.walk('./dataset/test_set/bicycles')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: bicycles")
+    result = predict(ret[0] + '/' + filename)
+    if result == 0:
+      bicycles_t += 1
+    else:
+      bicycles_f += 1
+
+for i, ret in enumerate(os.walk('./dataset/test_set/bridges')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: bridges")
+    result = predict(ret[0] + '/' + filename)
+    if result == 1:
+      bridges_t += 1
+    else:
+      bridges_f += 1
+
+for i, ret in enumerate(os.walk('./dataset/test_set/bus')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: bus")
+    result = predict(ret[0] + '/' + filename)
+    if result == 2:
+      bus_t += 1
+    else:
+      bus_f += 1
+
+for i, ret in enumerate(os.walk('./dataset/test_set/motorcycles')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: motorcycles")
+    result = predict(ret[0] + '/' + filename)
+    if result == 3:
+      motorcycles_t += 1
+    else:
+      motorcycles_f += 1
+
+for i, ret in enumerate(os.walk('./dataset/test_set/mountains_or_hills')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: mountains_or_hills")
+    result = predict(ret[0] + '/' + filename)
+    if result == 4:
+      mountains_or_hills_t += 1
+    else:
+      mountains_or_hills_f += 1
+
+for i, ret in enumerate(os.walk('./dataset/test_set/statues')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: statues")
+    result = predict(ret[0] + '/' + filename)
+    if result == 5:
+      statues_t += 1
+    else:
+      statues_f += 1
+    
+for i, ret in enumerate(os.walk('./dataset/test_set/storefront')):
+  for i, filename in enumerate(ret[2]):
+    if filename.startswith("."):
+      continue
+#    print("Label: storefront")
+    result = predict(ret[0] + '/' + filename)
+    print(result)
+    if result == 6:
+      storefront_t += 1
+    else:
+      storefront_f += 1
+"""
+Check metrics
+"""
+print("True bicycles: ", bicycles_t)
+print("False bicycles: ", bicycles_f)
+print("True bridges: ", bridges_t)
+print("False bridges: ", bridges_f)
+print("True bus: ", bus_t)
+print("False bus: ", bus_f)
+print("True motorcycles: ", motorcycles_t)
+print("False motorcycles: ", motorcycles_f)
+print("True mountains_or_hills: ", mountains_or_hills_t)
+print("False mountains_or_hills: ", mountains_or_hills_f)
+print("True statues: ", statues_t)
+print("False statues: ", statues_f)
+print("True storefront: ", storefront_t)
+print("False storefront: ", storefront_f)
