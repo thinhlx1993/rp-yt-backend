1ea3b122507dfe5b903c62c02159301d1fa547c6
unknown <thinh>
2018-09-07 11:35:07 +0700

    update

--- etc/setup/update.sh
+++ etc/setup/update.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-cd /opt/rp-yt-backend && git checkout /opt/rp-yt-backend/etc/setup/install.sh && git pull
+cd /opt/rp-yt-backend && git checkout /opt/rp-yt-backend/etc && git pull
 bash /opt/rp-yt-backend/etc/setup/install.sh
