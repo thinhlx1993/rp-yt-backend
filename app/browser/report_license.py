29726dfa758002281e4a8638399f6f34d2f9c7b6
unknown <thinh>
2018-09-07 10:43:14 +0700

    fully change mac address

--- app/browser/report_license.py
+++ app/browser/report_license.py
@@ -223,7 +223,7 @@ def change_language(browser):
 
 
 def fakeip():
-    subprocess.call(['macchanger', '-A', 'ens33'])
+    subprocess.call(['macchanger', '-r'])
     subprocess.call(['macchanger', '-s'])
     subprocess.call(['service', 'vpngate@worker', 'restart'])
     sleep(10)
